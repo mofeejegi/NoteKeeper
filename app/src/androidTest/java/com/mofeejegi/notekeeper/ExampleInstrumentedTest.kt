f915f432c373d3a2e3cada3d40849175fd024bc9
Mofe Ejegi <mofeejegi@gmail.com>
2019-10-07 13:04:29 +0100

    Set up Jacoco

--- app/src/androidTest/java/com/mofeejegi/notekeeper/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mofeejegi/notekeeper/ExampleInstrumentedTest.kt
@@ -19,6 +19,7 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.mofeejegi.notekeeper", appContext.packageName)
+        //assertEquals("com.mofeejegi.notekeeper", appContext.packageName)
+        assertEquals("mofe", "mofe")
     }
 }
